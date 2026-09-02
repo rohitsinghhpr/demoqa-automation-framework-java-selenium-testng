98db3d504eaf2d50d075d19e55de5bbbe0f07743
Harsh <rs907582@gmail.com>
2026-09-02 22:02:06 +0530

    modify maven.yml workflow

--- src/main/java/utils/FrameworkConstants.java
+++ src/main/java/utils/FrameworkConstants.java
@@ -1,7 +1,7 @@
 package utils;
 
 public class FrameworkConstants {
-    public static final boolean HEADLESS = false;
+    public static final boolean HEADLESS = true;
     public static final String PAGE_LOAD_STRATEGY = "normal";
     public static final String BROWSER = "chrome";
     public static final String BASE_URL = "https://demoqa.com/text-box";
